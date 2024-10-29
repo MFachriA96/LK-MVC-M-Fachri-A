e8ca6afd2d65e3876619e02cc58c7974d1a51f1c
MFachriA96 <149947273+MFachriA96@users.noreply.github.com>
2024-10-29 23:13:45 +0700

    Update Controller.php

--- Controller.php
+++ Controller.php
@@ -1,9 +1,17 @@
 <?php
-
-class Controller
-{
-    public function view($view, $data = [])
-    {
-        require_once "../app/views/$view.php";
+class Controller{
+    public function loadModel($model) {
+    if (file_exists('apps/models/' . $model . '.php')) {
+        require_once('apps/models/' . $model . '.php');
+        $model = new $model;
     }
+    return $model;
+} 
+public function loadview($view, $data = null) {
+    if (file_exists('apps/views/' . $view . 'php')) {
+        require_once('apps/views/' . $view . 'php');
+    }
+}
+
+
 }
