17830b58100980ca2ab4a17deed2b06d1008633c
MFachriA96 <149947273+MFachriA96@users.noreply.github.com>
2024-10-29 23:21:30 +0700

    Create index.php

---
+++ controllers/index.php
@@ -0,0 +1,13 @@
+<?php
+
+class Home {
+	public function __construct (){
+		echo "anda berada pada controller index\n";
+	}
+	public function index (){
+		echo "anda memanggil action index \n";
+	}
+	public function home (){
+		echo "anda memanggil action home \n";
+	}
+}
