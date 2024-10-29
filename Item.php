5850e26dee42cd0d4999b3db995d905c84acaad8
MFachriA96 <149947273+MFachriA96@users.noreply.github.com>
2024-10-29 22:23:31 +0700

    Create Item.php

---
+++ Item.php
@@ -0,0 +1,29 @@
+<?php
+
+class Item
+{
+    public static function getAll()
+    {
+        // Ambil semua item dari database
+    }
+
+    public static function create($data)
+    {
+        // Simpan data item baru ke database
+    }
+
+    public static function getById($id)
+    {
+        // Ambil item berdasarkan ID
+    }
+
+    public static function update($id, $data)
+    {
+        // Update item di database
+    }
+
+    public static function delete($id)
+    {
+        // Hapus item dari database
+    }
+}
