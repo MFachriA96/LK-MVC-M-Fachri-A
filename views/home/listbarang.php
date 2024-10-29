bb0505afe8ba72a28cb12aadcf58834c924c812e
MFachriA96 <149947273+MFachriA96@users.noreply.github.com>
2024-10-29 23:24:14 +0700

    Create listbarang.php

---
+++ views/home/listbarang.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <table>
+    <tr>
+        <th>ID</th>
+        <th>Nama Barang</th>
+        <th>QTY</th>
+    </tr>
+    <?php foreach ($data as $item) : ?>
+    <tr>
+    <td><?= $item['id'] ?></td>
+    <td><?= $item['nama'] ?></td>
+    <td><?= $item['qty'] ?></td>
+    </tr>
+<?php endforeach ?>
+    
+</table>
+
+</body>
+</html>
